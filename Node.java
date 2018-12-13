195a11096258f3b4e475f8f41fb379343add17e1
Sharon Zou <szou00@stuy.edu>
2018-12-13 02:11:53 -0500

    added set/get methods

--- Node.java
+++ Node.java
@@ -5,4 +5,35 @@ public class Node{
  public Node(int x) {
    data = x;
  }
+
+ public Node(int x, Node nextN, Node prevN) {
+   data = x;
+   next = nextN;
+   prev = prevN;
+ }
+
+ public int getData() {
+   return data;
+ }
+
+ public Node getNext() {
+   return next;
+ }
+
+ public Node getPrev() {
+   return prev;
+ }
+
+ public void setData(int newData) {
+   data = newData;
+ }
+
+ public void setPrev(Node newPrev) {
+   prev = newPrev;
+ }
+
+ public void setNext(Node newNext) {
+   next = newNext;
+ }
+ 
 }
