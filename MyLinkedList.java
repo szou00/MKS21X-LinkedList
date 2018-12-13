c166807ff53366fc18cd513a743ecd0f9c45f411
Sharon Zou <szou00@stuy.edu>
2018-12-13 02:37:44 -0500

    add updated

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -15,6 +15,12 @@ public class MyLinkedList {
  }
 
  public boolean add(int value) {
+   Node newVal = new Node(value);
+   end.setNext(newVal);
+   end = newVal; //the new Node becomes the last node
+   newVal.setPrev(end); //it gets the end as the previous and vice versa
+   size += 1; //size increases by one
+   return true;
  }
 
  public String toString() {
