ab9a8e1d013e3f801ca3bb39ff1d9142f9c75ea2
Sharon Zou <szou00@stuy.edu>
2018-12-18 04:20:32 -0500

    updated driver

--- Driver.java
+++ Driver.java
@@ -8,6 +8,12 @@ public class Driver {
       //list.add((int)(Math.random() * 100)); //adds in random numbers
       System.out.println(list);
     }
+    int[] list3 = {92, 12, -2, 53, -32};
+    MyLinkedList newlist3 = new MyLinkedList();
+    for (int x = 0; x < 5; x++) {
+      newlist3.add(list3[x]);
+      System.out.println(newlist3);
+    }
     System.out.println("Should print -75: " + list.get(5));
     System.out.println("Should print 7: " + list.set(3,28));
     System.out.println("The value at index 3 is now 28: " + list);
@@ -15,20 +21,20 @@ public class Driver {
     System.out.println("Should print 2, the index of 83: " + list.indexOf(83));
     System.out.println("Testing if the list contains -2, should print false: " + list.contains(-2));
     System.out.println("Should print -1, because the list doesn't contain -2: " + list.indexOf(-2));
-    list.add(2,Integer.valueOf(-9));
+    list.add(2,-9);
     System.out.println("-9 should be at index 2: " + list);
-    list.add(3,Integer.valueOf(17));
+    list.add(3,17);
     System.out.println("17 should be at index 3: " + list);
-    list.add(0,Integer.valueOf(1));
+    list.add(0,1);
     System.out.println("1 should be at index 0: " + list);
-    list.add(0,Integer.valueOf(0));
+    list.add(0,0);
     System.out.println("0 should be at index 0: " + list);
     System.out.println("Removing index 4, should print -9: " + list.remove(4));
     System.out.println(list);
-    System.out.println("Finding indexOf of 0: " + list.indexOf(0));
-    System.out.println("Removing value 0, should print true: " + list.remove(Integer.valueOf(0)));
+    System.out.println("Finding indexOf of 0: " + list.indexOf(new Integer (0)));
+    System.out.println("Removing value 0, should print true: " + list.remove(new Integer (0)));
     System.out.println(list);
-    System.out.println("Removing value 12, should print true: " + list.remove(Integer.valueOf(12)));
+    System.out.println("Removing value 12, should print true: " + list.remove(new Integer (12)));
     System.out.println(list);
 
     System.out.println("\n" + "Testing special cases");
@@ -38,5 +44,13 @@ public class Driver {
     System.out.println("Prints out 40: " + list2.get(0));
     System.out.println("Testing if the list contains 40, should print true: " + list2.contains(40));
     System.out.println("Should print 0, the index of 40: " + list2.indexOf(40));
+    list.extend(newlist3);
+    System.out.println("list extends list3: " + list);
+    System.out.println("size of list3, should be 0: " + newlist3.size());
+    System.out.println("size of list, should be 16: " + list.size());
+    System.out.println("start of list, should be 1: " + list.get(0));
+    System.out.println("end of list, should be -32: " + list.get(list.size() - 1));
+    list.extend(list2);
+    System.out.print(list);
   }
 }
